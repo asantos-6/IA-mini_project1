4a3ab932dd6e0102fd0c76579269d8351afbbba5
xia4991 <xab1994@gmail.com>
2017-10-16 01:52:41 +0800

    uma data de cenas

--- jj.py
+++ jj.py
@@ -20,6 +20,9 @@ class State:
     def get_element(self):
         return self.Elements
 
+    def get_launch(self):
+        return self.Launch
+
 
 
 
@@ -65,42 +68,53 @@ def read_doc(doc_name):
     return Vertices, Edges, launch_datas, G
 
 
-def find_all_next_states(actual_state, launched_nodes, adj_nodes, max_payload, act_weight, element_weight):
+def find_all_next_states(actual_state, launched_nodes, adj_nodes, max_payload, act_weight):
     next_states = []
 
     if (len(adj_nodes) > 0):
+        '''
         for x in range(0,len(adj_nodes)):
             if (element_weight[x] > max_payload):
                 adj_nodes.remove(adj_nodes[x])
                 element_weight.remove(element_weight[x])
+        '''
 
         for x in range(0, len(adj_nodes)):
-            new_state = create_state(actual_state.Launch,actual_state.Elements.append(adj_nodes[x]))
-            new_state.append(new_state)
+            new_elements = actual_state.get_element()
+            new_elements.append(adj_nodes[x])
+            current_state = State(actual_state.get_launch(),new_elements)
+            new_states.append(current_state)
+
             new_launched_nodes = list(launched_nodes)
             del launched_nodes[x]
+
             new_adj_nodes = list(adj_nodes)
             del adj_nodes[x]
-            new_element_weight = list(element_weight)
-            new_act_weight = act_weight + element_weight[x]
-            del element_weight[x]
-            next_states.append(find_all_next_states(new_state, new_launched_nodes, new_adj_nodes, max_payload, new_act_weight, element_weight))
+
+
+
+            new_act_weight = act_weight + float(PESOS[adj_nodes[x]])
+
+            next_states.append(find_all_next_states(current_state, new_launched_nodes, new_adj_nodes, max_payload, new_act_weight))
 
 
     return new_state
 
 
+def find_adj_node(node):
+    node_key = G[node]
+    node_list = []
+    for key in node_key.keys():
+        node_list.append(key)
+    print (node_list)
+    return node_list
 
 
 def main():
     V, E, L, G = read_doc(DOC)
     print(PESOS)
 
-    node_key = G['VCM']
-    node_list = []
-    for key in node_key.keys():
-        node_list.append(key)
-    print (node_list)
+    
 
     elements = ['VCM']
     init = State(1,elements)
