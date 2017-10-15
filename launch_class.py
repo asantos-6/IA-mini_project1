99fa4834f981a3c8e5a06bab6116bccb06d9fcb0
SongaMonga <32817093+SongaMonga@users.noreply.github.com>
2017-10-15 17:02:10 +0100

    Graph search e launch class

---
+++ launch_class.py
@@ -0,0 +1,9 @@
+#filename: launch_class.py
+
+class Launch:
+    def __init__(self, li, ti, pi, fi, ui):
+        self.li=li
+        self.ti=ti
+        self.pi=pi
+        self.fi=fi
+        self.ui=ui
