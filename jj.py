802922fdd8d122b34b7cf8c977485c9fc9dbb624
xia4991 <xab1994@gmail.com>
2017-10-18 03:49:10 +0800

    preciso de grandes modificacoes para que isto funcione na globalidade, e melhor esperar pela tua solucao para nao trabalhar em paralelo

--- jj.py
+++ jj.py
@@ -17,6 +17,7 @@ class State:
     def __init__(self, launch, elements_on_space):
         self.Launch = launch
         self.Elements = elements_on_space
+        self.Cost = 0
 
         return
 
@@ -51,6 +52,10 @@ def read_doc(doc_name):
     Weight = []                     #vetor de peso de componentes de satelite
     launch_datas = []               #lista de lista onde contem as informacoes acerca de cada launch, cada lista contem max weight, fixed cost e variable cost
 
+    launch_info1 = []
+    launch_info2 = []
+    launch_info3 = []
+    
     f = open(doc_name)
     line = f.readline()
     while line:
@@ -69,11 +74,15 @@ def read_doc(doc_name):
                 G.add_edge(*edge)
                 Edges.append(edge_pair)
             if(words[0][0] == 'L'):
-                launch_info = []
-                launch_info.append(words[2])
-                launch_info.append(words[3])
-                launch_info.append(words[4])
+                launch_info1.append(float(words[2]))
+                launch_info2.append(float(words[3]))
+                launch_info3.append(float(words[4]))
         line = f.readline()
+
+    launch_datas.append(launch_info1)
+    launch_datas.append(launch_info2)
+    launch_datas.append(launch_info3)
+    
 
     '''
     nx.draw(G,with_labels = True)
@@ -86,13 +95,16 @@ def read_doc(doc_name):
     return Vertices, Edges, launch_datas, G
 
 
+def isInList(list_a, b):
+    for e in list_a:
+        if State.compareState(e,b):
+            return True
+    return False
 
 def addInexistenceState(list_a, list_b):
-    list_out = list(list_a)
     for s in list_b:
-        for a in list_a:
-            if not State.compareState(a,s):
-                list_out.extend()
+        if not isInList(list_a,s):
+            list_a.append(s)
 
 
 
@@ -100,44 +112,34 @@ def find_all_next_states(actual_state, launched_nodes, adj_nodes, max_payload, a
     next_states = []
 
     if (len(adj_nodes) > 0):
-        '''
-        for x in range(0,len(adj_nodes)):
-            if (element_weight[x] > max_payload):
-                adj_nodes.remove(adj_nodes[x])
-                element_weight.remove(element_weight[x])
-        '''
+        
         new_elements = actual_state.get_element()
         for x in range(0, len(adj_nodes)):
-            if (len(adj_nodes) ==3):
-                print ("--------------primeira chamada---------------", new_elements)
-            print ("-----------------------------",adj_nodes[x])
-            #actual_state.print_state()
-            new_elements = list(actual_state.get_element())
-            new_elements.append(adj_nodes[x])
-            #print ("list",new_elements)
-            new_elements1 = list(new_elements)
-            current_state = State(actual_state.get_launch(),new_elements1)
-            #current_state.print_state()
-            next_states.append(current_state)
-            del new_elements[-1]
-            #current_state.print_state()
+            component_weight = PESOS[adj_nodes[x]]
+            if ((act_weight+component_weight <= max_payload)):
+                    if (len(adj_nodes) ==3):
+                        print ("--------------primeira chamada---------------", new_elements)
+                    
+                    new_elements = list(actual_state.get_element())
+                    new_elements.append(adj_nodes[x])
+                    new_elements1 = list(new_elements)
+                    current_state = State(actual_state.get_launch(),new_elements1)
+                    next_states.append(current_state)
+                    del new_elements[-1]
 
-            print (current_state.getter())
-            
-            new_launched_nodes = list(launched_nodes)
-            new_launched_nodes.append(adj_nodes[x])
+                    print (current_state.getter())
+                    
+                    new_launched_nodes = list(launched_nodes)
+                    new_launched_nodes.append(adj_nodes[x])
 
-            new_adj_nodes = list(adj_nodes)
-            del new_adj_nodes[x]
+                    new_adj_nodes = list(adj_nodes)
+                    del new_adj_nodes[x]
 
-            adj_list = find_adj_node(adj_nodes[x])
+                    adj_list = find_adj_node(adj_nodes[x])
 
-            #for node in adj_list:
+                    new_act_weight = act_weight + float(PESOS[adj_nodes[x]])
 
-
-            new_act_weight = act_weight + float(PESOS[adj_nodes[x]])
-
-            next_states.extend(find_all_next_states(current_state, new_launched_nodes, new_adj_nodes, max_payload, new_act_weight))
+                    addInexistenceState(next_states,find_all_next_states(current_state, new_launched_nodes, new_adj_nodes, max_payload, new_act_weight))
 
     return next_states
 
@@ -156,27 +158,24 @@ def compare(s,t):
 
 
 
+def findAllLaunchStates(actual_state, launched_nodes, adj_nodes, total_launch,launch_info):
+    all_states = []
+    all_states_in_launch = []
+    all_states_in_launch.append(actual_state)
+    for x in range(0,total_launch):
+        for y in range(0,len(all_states_in_launch)):
+            states = find_all_next_states(all_states_in_launch[y], launched_nodes, adj_nodes, launch_info[x][0], 0)
+    print (len(states))
+    for a in states:
+        a.print_state()
+
 def main():
     V, E, L, G = read_doc(DOC)
     print(PESOS)
 
-    
-
-    init = State(1,['VCM'])
-    print (init.getter())
-
-    node_list = find_adj_node('VCM')
-
-    print (node_list)
-    node_list = ['VS', 'VK1', 'VK2']
-    all_states = find_all_next_states(init, init.get_element(), node_list, 22.8, 5)
-    print (len(all_states))
-    for a in all_states:
-        a.print_state()
-
-    print (init)
-
-    print (State.compareState(all_states[2],all_states[4]))
+    init = State(1,[])
+    node_list = ['VS', 'VK1', 'VK2', 'VP', 'VPM', 'VSTM', 'VK', 'VDM']
+    findAllLaunchStates(init,  init.get_element(), node_list,1,L)
 
 
 
