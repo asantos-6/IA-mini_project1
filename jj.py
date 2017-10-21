098eb2c0632a87a01450c96a15636b3e3e5dcaa5
SongaMonga <32817093+SongaMonga@users.noreply.github.com>
2017-10-22 00:15:21 +0100

    quase a conseguir retornar todos os estados possíveis sendo dado o estado init

--- jj.py
+++ jj.py
@@ -21,6 +21,8 @@ class Element:
 
     def get_element(self):
         return self.ID
+    def get_weight(self):
+        return self.weight
 
 class Edge:
     def __init__(self, element1, element2):
@@ -46,9 +48,11 @@ class Launch:
 
 
 class State:
-    def __init__(self, launch, elements):
+    def __init__(self, launch, elements): #''', path, cost):
         self.launch = launch
         self.elements = elements
+        #self.path = path
+        #self.cost = cost
 
         return
 
@@ -116,40 +120,140 @@ def read_doc(doc_name):
     Launches.sort(key = lambda r: r.launch_date)
 
     for e in STATION_ELEMENTS:
-        e.adj_list = find_adj_node(e.get_element())
+        e.adj_list = find_adj_node(e.ID)
     #for x in range(0,len(Vertices)):
         #PESOS[Vertices[x]] = Weight[x]
 
     return STATION_ELEMENTS, Edges, Launches, STATION_PLANT
 
 
-def find_all_next_states(previous_state, actual_node, max_payload):
-    if previous_state.get_info("l") == 0:
+def expand_state(previous_state, max_payload):
+    next_states = []
+    if previous_state.launch == 0:
+        previous_element_list = []
+
+        #for e in STATION_ELEMENTS
         open_list = STATION_ELEMENTS
     else:
-        open_list = previous_state.get_info("e")
-
-        '''mudar esta merda
-
-    if current_weight < max_payload:
-
+        previous_element_list = previous_state.elements
+        for y in previous_element_list:
+            for z in previous_element_list:
+                if z != y:
+                    open_list = []
+                    open_list.extend(disjoint_union(z.adj_list, y.adj_list))
+    next_states = combine_elements(previous_state.launch+1, open_list, previous_element_list, next_states, max_payload)
 
     return next_states
+
+def combine_elements (launch, elements, previous_element_list, next_states, max_payload):
+    total_weight = 0
+    new_elements = []
     '''
+    print("Elements:")
+    for e in elements:
+        print(e.ID)
+
+    print("Previous Element List:")
+    for e in previous_element_list:
+        print(e.ID)
+    print("")
+    '''
+    for e in elements:
+        #print(e.ID)
+        new_states = []
+        total_weight = e.weight
+        for x in previous_element_list:
+            total_weight = total_weight + x.weight
+
+        if total_weight < max_payload:
+            new_elements = list(previous_element_list)
+            new_states = list(next_states)
+
+            already_there = 0
+            for x in new_elements:
+                if x.ID == e.ID:
+                    already_there = 1
+                    break
+            if not already_there:
+                new_elements.append(e)
+            '''
+            print("New Elements:")
+            for e in new_elements:
+                print(e.ID)
+            print("")
+            '''
+            #for x in new_elements:
+                #print(x.ID)
+            new_state = State(launch, new_elements)
+            new_state.elements.sort(key=lambda x: x.ID)
+            #elements = elements.remove(e)
+            #elements = []
+            if len(new_elements) == 1:
+                elements = list(new_elements[0].adj_list)
+            else:
+                elements = []
+                i = 0
+                elements = disjoint_union(new_elements[0].adj_list, new_elements[1].adj_list)
+
+                for i in range(2, len(new_elements)):
+                    elements = disjoint_union(new_elements[i].adj_list, elements)
+
+                for y in new_elements:
+                    elements = list(filter(lambda x: x.ID != y.ID, elements))
+            #for x in elements:
+                #print(x.ID)
+            for e in elements:
+                if not e.adj_list:
+                    e.adj_list = find_adj_node(e.ID)
+            already_there = 0
+            for n in new_states:
+                if new_state == n:
+                    already_there = 1
+                    break
+            if not already_there:
+                new_states.append(new_state)
+            '''
+            print("New States:")
+            for n in new_states:
+                print(n.launch, end = ": ")
+                for e in n.elements:
+                    print(e.ID, end = " ")
+                print("")
+            print("")
+            '''
+            new_states.extend(combine_elements(launch, elements, new_elements, new_states, max_payload))
+
+    if not new_elements or already_there:
+        return []
+    else:
+        return new_states
 
 def find_adj_node(node):
     node_key = STATION_PLANT[node]
     node_list = []
+    elements = list(STATION_ELEMENTS)
     for key in node_key.keys():
-        node_list.append(key)
-    #print (node_list)
+        for e in elements:
+            if e.ID == key:
+                element = Element(key, float(e.weight))
+                node_list.append(element)
+                elements.remove(e)
+                break
     return node_list
 
 
 def main():
     STATION_ELEMENTS, E, L, STATION_PLANT = read_doc(DOC)
-    #print(PESOS)
 
+    init = State(0, STATION_ELEMENTS)
+    first_launch = expand_state(init, 140)
+    for x in first_launch:
+        print(x.launch, end = ':')
+        for e in x.elements:
+            print(" ", e.ID, end = ',')
+        print("")
+
+    #(PESOS)
     #for e in STATION_ELEMENTS:
         #print(e.get_element(), "->", e.adj_list)
     #print(STATION_PLANT)
