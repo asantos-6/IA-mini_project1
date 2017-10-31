cd2bac8974544be05d3391df5127f98bd89eecab
xia4991 <xab1994@gmail.com>
2017-10-31 13:42:59 +0800

    separar as funcoes

--- jj.py
+++ jj.py
@@ -5,7 +5,7 @@ from collections import Counter
 import copy
 import scipy as sp
 import numpy as np
-import State
+from State import State
 
 MAX = 999999
 MAX_PRICE = 0
@@ -22,114 +22,115 @@ def increment():
     BUG += 1
     return
 
-class State:
 
-    def __init__(self, launch, elements_on_space):
-        self.Launch = launch
-        self.Elements = elements_on_space
-        self.path = []
-        self.Cost = []
+# class State:
 
-        return
+#     def __init__(self, launch, elements_on_space):
+#         self.Launch = launch
+#         self.Elements = elements_on_space
+#         self.path = []
+#         self.Cost = []
 
-    def getter(self):
-        a = [self.Launch,self.Elements]
-        return a
+#         return
 
-    def get_element(self):
-        return self.Elements
+#     def getter(self):
+#         a = [self.Launch,self.Elements]
+#         return a
 
-    def get_launch(self):
-        return self.Launch
+#     def get_element(self):
+#         return self.Elements
 
-    def get_path(self):
-        return self.path
-    def get_path_at(self, index):
-        return self.path[index]
+#     def get_launch(self):
+#         return self.Launch
 
-    def get_cost(self):
-        return self.Cost
+#     def get_path(self):
+#         return self.path
+#     def get_path_at(self, index):
+#         return self.path[index]
 
-    def get_total_cost(self):
-        total_cost = 0
-        for c in self.Cost:
-            total_cost += c
-        return total_cost
+#     def get_cost(self):
+#         return self.Cost
 
-
-    def print_state(self):
-        total_cost = 0
-        for a in self.Cost:
-            total_cost += a
-        print ("state print:",self.Launch, self.Elements,"      path:", self.path, "    cost:", self.Cost, "    total_cost:", total_cost)
-        return
+#     def get_total_cost(self):
+#         total_cost = 0
+#         for c in self.Cost:
+#             total_cost += c
+#         return total_cost
 
 
-    def compare(s,t):
-        return Counter(s) == Counter(t)
-
-    def compareState(a,b):
-        if(a.Launch == b.Launch & compare(a.Elements,b.Elements)):
-            return True
-        else:
-            return False
-
-    def is_repeat(node_a, node_b):
-        if node_a.Launch != node_b.Launch:
-            return False
-        if (len(node_a.Elements) != len(node_b.Elements)):
-            return False
-
-        if (set(node_a.Elements) == set(node_b.Elements)):
-            return True
-
-        return False
-
-    def increment_launch(self):
-        self.Launch += 1
-
-    def save_path(self, past_path):
-        self.path = past_path
-
-    def save_cost(self, cost):
-        self.Cost = cost       
+#     def print_state(self):
+#         total_cost = 0
+#         for a in self.Cost:
+#             total_cost += a
+#         print ("state print:",self.Launch, self.Elements,"      path:", self.path, "    cost:", self.Cost, "    total_cost:", total_cost)
+#         return
 
 
-    def set_path(self, new_path):
-        self.path = new_path
+#     def compare(s,t):
+#         return Counter(s) == Counter(t)
 
-    def append_cost(self,cost):
-        self.Cost = cost
+#     def compareState(a,b):
+#         if(a.Launch == b.Launch & compare(a.Elements,b.Elements)):
+#             return True
+#         else:
+#             return False
+
+#     def is_repeat(node_a, node_b):
+#         if node_a.Launch != node_b.Launch:
+#             return False
+#         if (len(node_a.Elements) != len(node_b.Elements)):
+#             return False
+
+#         if (set(node_a.Elements) == set(node_b.Elements)):
+#             return True
+
+#         return False
+
+#     def increment_launch(self):
+#         self.Launch += 1
+
+#     def save_path(self, past_path):
+#         self.path = past_path
+
+#     def save_cost(self, cost):
+#         self.Cost = cost       
 
 
-    def actualize(self,previous_path):
-        self.path = previous_path
-        new_path = []
-        i = 0
-        for a in self.path:             #conta quantidades de elementos existentes em path. ou seja, numero de componentes ja lancados
-            for b in a:
-                i += 1 
-        n = len(self.Elements) - i      #obtem se o numero de componentes que vai ser lancado neste launch
-        if n == 0:
-            previous_path.append([])
-        if (n > 0):
-            for x in range(i,len(self.Elements)):
-                new_path.append(self.Elements[x])
-            previous_path.append(new_path)
+#     def set_path(self, new_path):
+#         self.path = new_path
 
-        self.path = previous_path
+#     def append_cost(self,cost):
+#         self.Cost = cost
 
-    def cost_is_higher(node_a, node_b):
-        cost_a = 0
-        cost_b = 0
-        for c in node_a.Cost:
-            cost_a += c
-        for c in node_b.Cost:
-            cost_b += c
-        if cost_a > cost_b:
-            return False
-        else:
-            return True
+
+#     def actualize(self,previous_path):
+#         self.path = previous_path
+#         new_path = []
+#         i = 0
+#         for a in self.path:             #conta quantidades de elementos existentes em path. ou seja, numero de componentes ja lancados
+#             for b in a:
+#                 i += 1 
+#         n = len(self.Elements) - i      #obtem se o numero de componentes que vai ser lancado neste launch
+#         if n == 0:
+#             previous_path.append([])
+#         if (n > 0):
+#             for x in range(i,len(self.Elements)):
+#                 new_path.append(self.Elements[x])
+#             previous_path.append(new_path)
+
+#         self.path = previous_path
+
+#     def cost_is_higher(node_a, node_b):
+#         cost_a = 0
+#         cost_b = 0
+#         for c in node_a.Cost:
+#             cost_a += c
+#         for c in node_b.Cost:
+#             cost_b += c
+#         if cost_a > cost_b:
+#             return False
+#         else:
+#             return True
 
     
 
