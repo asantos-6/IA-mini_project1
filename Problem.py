05a6cb4a72ab566801a07d2deb97fa06bbe59ab6
xia4991 <xab1994@gmail.com>
2017-11-03 11:20:40 +0800

    tudo

--- Problem.py
+++ Problem.py
@@ -1,9 +1,11 @@
 
 class problem:
-	def __init__(self, init_state, successor, path_cost):
+	def __init__(self, init_state, successor, path_cost, check, add_state):
 		self.Init_state = init_state
 		self.Successor = successor
 		self.Path_cost = 0
+		self.Check_Goal = check
+		self.Add_state = add_state
 
 	def get_init(self):
 		return self.Init_state
---
+++ independent.py
@@ -0,0 +1,23 @@
+
+
+#General_search: General search algorithm (problem independent)
+#Arguments: Problem definition, strategy to implement
+def General_search(problem_1, strategy,PESOS,HEURISTIC_VALUE):
+    open_list= []
+    close_list = []
+    open_list.append(problem_1.get_init())
+    flag = 1
+
+    successor = problem_1.get_successor()
+    while(flag):
+        if not open_list:
+            return False
+        expansion_node = strategy(open_list, PESOS, HEURISTIC_VALUE)
+        #print ("expande node,", flag,":-------------", "lenght of open_list:", len(open_list))
+        #expansion_node.print_state()
+        if (problem_1.Check_Goal(expansion_node)):
+            return expansion_node, flag
+        else:
+            child_nodes = successor(expansion_node)
+            problem_1.Add_state(open_list, child_nodes)
+        flag += 1
